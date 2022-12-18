be30d0b1e66f64c9c6772b8c472b2a4f58ec88c3
nikkifixx <iden.nick@gmail.com>
2022-12-18 14:31:11 -0500

    Initial Commit

---
+++ TravelManagement/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TravelManagement
+//
+//  Created by Nick Iden on 2022-12-18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
